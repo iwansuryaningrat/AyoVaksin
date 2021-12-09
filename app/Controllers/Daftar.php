042c2446718e140da40ff90bfb3f02d042284afa
iwansuryaningrat <iwan.suryaningrat28@gmail.com>
2021-12-09 19:11:51 +0700

    Create Daftar Controller

---
+++ app/Controllers/Daftar.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Models\PendaftaranModel;
+use App\Models\PenerimavaksinModel;
+use App\Models\PenyediaModel;
+use App\Models\SaranModel;
+use App\Models\SertifikatModel;
+use App\Models\TiketModel;
+use App\Models\UsersModel;
+use App\Models\VaksinModel;
+
+class Daftar extends BaseController
+{
+    protected $pendaftaranModel;
+    protected $penerimavaksinModel;
+    protected $penyediaModel;
+    protected $saranModel;
+    protected $sertifikatModel;
+    protected $tiketModel;
+    protected $usersModel;
+    protected $vaksinModel;
+
+    public function __construct()
+    {
+        $this->pendaftaranModel = new PendaftaranModel();
+        $this->penerimavaksinModel = new PenerimavaksinModel();
+        $this->penyediaModel = new PenyediaModel();
+        $this->saranModel = new SaranModel();
+        $this->sertifikatModel = new SertifikatModel();
+        $this->tiketModel = new TiketModel();
+        $this->usersModel = new UsersModel();
+        $this->vaksinModel = new VaksinModel();
+    }
+}
